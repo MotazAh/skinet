d62d02131403b78ab3af2aad823831b088d73f84
MotazAh <almotaz.ahmed@gmail.com>
2024-01-25 19:37:41 +0200

    Added Routing And Lazy Loading

--- client/src/app/shop/shop.service.ts
+++ client/src/app/shop/shop.service.ts
@@ -27,6 +27,10 @@ export class ShopService {
     return this.http.get<Pagination<Product[]>>(this.baseUrl + 'products', {params}); // Can do {params: params} too unless variable name is different
   }
 
+  getProduct(id: number) {
+    return this.http.get<Product>(this.baseUrl + 'products/' + id);
+  }
+
   getBrands() {
     return this.http.get<Brand[]>(this.baseUrl + 'products/brands');
   }
