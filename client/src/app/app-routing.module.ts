a206dc9732d4a7a251f89f7275ad4326d8c613a1
MotazAh <almotaz.ahmed@gmail.com>
2024-01-26 20:14:32 +0200

    Improved Style And Implemented Loading Indicator

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -6,7 +6,7 @@ import { NotFoundComponent } from './core/not-found/not-found.component';
 import { ServerErrorComponent } from './core/server-error/server-error.component';
 
 const routes: Routes = [
-  {path: '', component: HomeComponent},
+  {path: '', component: HomeComponent, data: {breadcrumb: 'Home'}}, // Breadcrumb: 'Home' forces the small navigation on top right to contain Home link
   {path: 'test-error', component: TestErrorComponent},
   {path: 'not-found', component: NotFoundComponent},
   {path: 'server-error', component: ServerErrorComponent},
